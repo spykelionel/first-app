18307cf0eca9b2e11cf45fa67c36f3f116232ac9
spykelionel <social.lionel@gmail.com>
2024-03-09 11:25:15 +0100

    add users route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Carbon\Factory;
 
@@ -23,3 +24,4 @@ Route::get("/hello", function () {
     return view("hello", ["name" => $name]);
 });
 
+Route::get("/users", [UserController::class, 'index'])->name('users.index');

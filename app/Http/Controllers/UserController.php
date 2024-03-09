cf7601fcf53eaccdb8d3f78fbacf680768f83bf7
spykelionel <social.lionel@gmail.com>
2024-03-09 12:45:43 +0100

    add create and edit methiods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view("users.index");
+        $users = User::all();
+        return view("users.index",  ["users"=>$users]);
+    }
+    public function create(){
+        return view("users.create");
+    }
+    public function store(Request $req){
+        $data = $req->validate([
+            "name"=>"required",
+            "email"=>'required',
+            "password"=>"required"
+        ]);
+        $user = User::create($data);
+        return redirect(route("users.index"));
+   }
+   public function edit(User $user){
+        return view("users.edit", ['user'=>$user]);
+   }
+   public function update(User $user, Request $req){
+        $data = $req->validate([
+            "name"=>"required",
+            "email"=>'required',
+        ]);
+        
+        $user->update($data);
+        return redirect(route("users.index"))->with("success", "User $user->name has updated succesfully");
     }
 }

fec60dcf1e39c9fc2ffb3a21a8466f2741f23547
spykelionel <social.lionel@gmail.com>
2024-03-09 12:46:12 +0100

    add view for view all users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,5 +7,17 @@
 </head>
 <body>
     <p>Users Page</p>
+    <div>
+        <p>Users</p>
+        @if(session()->has('success'))
+            <p class="text-success">{{session('success')}}</p>
+        @endif
+        <p>Create new <a href="{{route('users.create')}}">User</a></p>
+       <ul>
+            @foreach($users as $user)
+                <li>{{$user->name}}, <a href="{{route('users.edit', ['user'=>$user])}}">Edit User</a></li>
+            @endforeach
+       </ul>
+    </div>
 </body>
 </html>
